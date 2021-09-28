fc85c1ead458500df5f6b00281bfa93a021729f6
nachi739 <namiki081025@gmail.com>
2021-09-28 19:28:42 +0900

    code12_07

---
+++ 12章/src/code12_07/Hero.java
@@ -0,0 +1,22 @@
+package code12_07;
+
+public class Hero extends Character {
+	String name = "ミナト";
+	int hp = 100;
+	//転ぶ
+	public final void slip() {
+		this.hp -= 5;
+		System.out.println(this.name + "は転んだ");
+		System.out.println("5のダメージ");
+	}
+	//戦う
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージを与えた");
+	}
+	//逃げる
+	public void run() {
+		System.out.println(this.name + "は逃げ出した");
+	}
+}
---
+++ 12章/src/code12_07/Thief.java
@@ -0,0 +1,10 @@
+package code12_07;
+
+public class Thief extends Character {
+	String name = "ササキ";
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃");
+		System.out.println("敵に4ポイントのダメージ");
+		m.hp -= 4;
+	}
+}
